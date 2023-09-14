6e0109e9d922893cf1eb9a4be210703942e41f77
JuanDi <juandixarteagaperez210@gmail.com>
2023-09-14 17:45:28 -0500

    Initial design of presentation

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { PresentationComponent } from './presentation/presentation.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    PresentationComponent
   ],
   imports: [
     CommonModule
